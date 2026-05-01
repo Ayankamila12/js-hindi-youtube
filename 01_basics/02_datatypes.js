5912e3b2c7a7eb0cdca9bfb2036321d93fa67481
Ayankamila12 <ayankamila031@gmail.com>
2026-05-01 10:31:08 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";//treat all js code as newer version
+
+//alert(3 + 3)// we are using nodejs,not browser
+let name="hitesh"
+let age=23
+let isloggedin=false
+let state=null
+//number =>2 to power 53
+//bigint
+//string=> ""
+//boolean=>true/false
+//null=>standalone value
+//undefined
+//symbol =>unique
+
+
+//object
+
+console.log(typeof age)
