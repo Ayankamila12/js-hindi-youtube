a3c59be6442515973b1663dbcbc819ea2f95dbf9
Ayankamila12 <ayankamila031@gmail.com>
2026-05-01 13:57:17 +0000

    datatypes convertion

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,52 @@
+//let score="33"
+//let s="33abc"
+//console.log(typeof (score))
+//let valueinnumber=Number(score)
+//let val=Number(s)
+//console.log(typeof valueinnumber)
+//console.log(val)
+// "33" =>33
+//"33abc"=>NaN
+//"true"=>1
+//false=>0
+//let isloggedin=1
+//let booleanislooggedin=Boolean(isloggedin)
+//console.log(booleanislooggedin)
+//console.log(typeof booleanislooggedin)
+ //1 =>true
+ //0=>false
+ //""=>false
+ //"ayan"=>true
+//  let somenum=33
+ //let stringnum=String(somenum)
+ //console.log(stringnum)
+ //console.log(typeof stringnum)
+
+ // *******operations *********//
+ let value=3
+ let negvalue=-value
+//console.log(negvalue) 
+// console.log(2 + 2) =>4
+// console.log(2-2) =>0
+// console.log(2 ** 3) =>8
+// console.log(2*2) =>4
+// console.log(2/2) =>1
+// console.log(2%2) =>0
+let str1="hello"
+let str2=" ayan"
+let str3=str1 + str2
+console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2+2)
+// console.log(1+2+"2")
+// console.log(+true)
+// console.log(+"")
+
+
+let num1,num2,num3
+num1=num2= num3=2+2
+let gamecounter=100
+++gamecounter
+console.log(gamecounter)
