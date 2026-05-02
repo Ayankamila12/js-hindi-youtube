afe714d9321925338724b216cd1d906ec0c2e994
Ayankamila12 <ayankamila031@gmail.com>
2026-05-02 04:06:58 +0000

    datatypes summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,29 @@
+//premitive
+
+//7 types : String,Number,Boolean,null,undefined,symbol,Bigint
+const score=100
+const score1=100.1
+const isloggin=false
+const uytsidetemp=null
+let useremail
+
+const id =Symbol('123')
+const anotherid=sybol('123')
+console.log(id == anotherid);
+const bignumber=35453651534655646756n
+
+
+
+//reference(non-premitive)
+//arrays,objects,functions
+const heroes=["shaktiman","doga"]
+let myobj={
+    name:"hitesh",
+    age:22,
+
+}
+const myfunction=function(){
+    console.log("Hello world")
+}
+
+console.log(typeof bignumber)
