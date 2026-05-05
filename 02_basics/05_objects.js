37374ce53636c995eff03d36381cea1b6c53073d
Ayankamila12 <ayankamila031@gmail.com>
2026-05-05 16:37:40 +0000

    objects and function

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,17 @@
+const course={
+    coursename:"jsinhindi",
+    price:"999",
+    courseinstructor:"Hitesh"
+}
+
+const {courseinstructor:instructor}=course
+console.log(instructor)
+const {price}=course
+console.log(price)
+console.log(course.courseinstructor)
+
+// {
+//     "name":"Ayan",
+//     "coursename1":"hindi",
+//    "price1":"free"
+// }
---
+++ 03_basics/01_functions.js
@@ -0,0 +1,22 @@
+function saymyname(){
+    console.log("A")
+    console.log("Y")
+    console.log("A")
+    console.log("N")
+}
+// function addtwonumbers(num1,num2){
+//     console.log(num1+num2)
+// }
+//saymyname()
+
+// 
+function addtwonumbers(num1,num2){
+//     let result=num1 + num2
+//     return result
+//     console.log("Ayan") // after return statement any line do not be executed
+        return num1 + num2
+ }
+const result=addtwonumbers(3,5)
+console.log(result)
+
+
